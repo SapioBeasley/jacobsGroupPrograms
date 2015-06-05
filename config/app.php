1040c7667392e861481918f24b6b281185f78340
Andreas Beasley <andreas@sapioweb.com>
2015-06-04 23:39:34 -0700

    done

--- config/app.php
+++ config/app.php
@@ -144,6 +144,8 @@ return [
 		'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
+		'Spatie\Tail\TailServiceProvider',
+		'Illuminate\Html\HtmlServiceProvider',
 
 	],
 
@@ -192,6 +194,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+		'Form'=> 'Illuminate\Html\FormFacade',
+		'HTML'=> 'Illuminate\Html\HtmlFacade',
 
 	],
 
